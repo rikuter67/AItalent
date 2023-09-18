cc713d50f1a840763d57d452c5799f1295174fde
rikuter67 <rikuto.01.05.04@gmail.com>
2023-09-18 12:23:28 +0900

    commnet

--- Main.py
+++ Main.py
@@ -7,12 +7,12 @@ from text_samples import cafe_example1, cafe_example2, cafe_example3, travel_exa
 from comment_samples import comment_sample1, comment_sample2, comment_sample3, comment_sample4, comment_sample5, comment_sample6, comment_sample7, comment_sample8, comment_sample9
 
 # 環境変数にAPIキーを設定
-openai.api_key = st.secrets["openai_key"]
+openai.api_key = env.openai_key
 
 # セッション状態を管理するためのクラス
 class State:
     def __init__(self):
-        self.comments_replies = []
+        self.comments_replies = {}  # ディクショナリとして初期化
 
 # セッション状態を取得または初期化
 def get_state():
@@ -20,6 +20,18 @@ def get_state():
         st.session_state['state'] = State()
     return st.session_state['state']
 
+# 既存のユーザーのコメントと返信を取得する関数
+def get_user_history(username, state):
+    if username in state.comments_replies:
+        return state.comments_replies[username]
+    else:
+        state.comments_replies[username] = {
+            'comments': [],
+            'replies': [],
+            'messages': []
+        }
+        return state.comments_replies[username]
+    
 # Streamlitアプリの本体
 def app():
     # Streamlitセッション状態を初期化
@@ -132,7 +144,7 @@ def app():
 
         # GPT-3 API呼び出し
         response = openai.ChatCompletion.create(
-            model="gpt-4",
+            model="gpt-3.5-turbo",
             messages=[
                 {"role": "system", "content": f"""
                 あなたはAIタレントのゆいです。Instagramで活動しています。
@@ -207,88 +219,82 @@ def app():
         if 'user_comment' not in st.session_state:
             st.session_state.user_comment = ""
 
-        # コメント入力用のテキストボックスを表示
-        st.subheader("コメント:")
-        st.session_state.user_comment = st.text_input("コメントを入力してください:", value=st.session_state.user_comment)
 
-        messeage = [
-                {"role": "system", "content": """
-                あなたはAIタレントのゆいです。Instagramで活動しています。
-                投稿に対するフォロワーからのコメントに対して返信をするように依頼されました。
-                以下にあなたに関する情報を記載しますので、それを参考にして返信をしてください。
+        # ユーザー名入力
+        default_value = "@"
+        username = st.text_input("ユーザー名を入力してください", default_value)
+
+        if username: # ユーザー名が入力されている場合のみ以下の処理を行う
+            # セッション状態を取得
+            state = get_state()
+
+            # 既存のユーザーのコメントと返信を取得
+            user_history = get_user_history(username, state)
+
+            # コメント入力と送信
+            st.subheader("コメント:")
+            st.session_state.user_comment = st.text_input("コメントを入力してください:", value=st.session_state.user_comment)
+        
+            messeage = [
+                    {"role": "system", "content": f"""
+                    あなたはAIタレントのゆいです。Instagramで活動しています。
+                    
+                    # 詳細プロフィール
+                    名前: 中村 ゆい (Nakamura Yui)
+                    誕生日: 2000年7月18日
+                    年齢: 23歳
+                    性別: 女性
+                    MBTI: ISFP
+                    居住地: 東京都中野区
+                    出身地: 千葉県
+                    職業: バリスタ
+                    Instagramハンドル: @YuiCafeTokyo
+                     
+                    最近投稿した文章は以下の通りです。："{st.session_state.choices[st.session_state.selected_index]}"
+                    """}
+            ]
+
+            # ここで会話の履歴を追加
+            for message in user_history['messages']:
+                messeage.append(message)
+
+            if st.button('コメント送信', key="submit_comment_button"):
+                # GPT-3による返答生成
+                comment_prompt = f"""
+                あなたの最近のインスタグラムの投稿に、フォロワーから次のようなコメントが寄せられた。
+                ユーザーからのコメントコメント: "{st.session_state.user_comment}"
+
+                このコメントに対して、必要ならプロフィールや投稿文章、会話履歴を用いて返信してください。
+                """
+
+                messeage.append({"role": "user", "content": comment_prompt})
+
+
+                # GPT-3 API呼び出し
+                comment_response = openai.ChatCompletion.create(
+                model="gpt-4",
+                messages=messeage,
+                max_tokens=200,  # 200文字以内の返答に制限
+            )
+
+                # 生成されたテキストを取得して表示（と保存）
+                generated_reply = comment_response['choices'][0]['message']['content']
                 
-                # 詳細プロフィール
+                # 新しい返信を追加
+                user_history['replies'].append(generated_reply)
+                # 新しいコメントを追加
+                user_history['comments'].append(st.session_state.user_comment)
+                # 新しいメッセージを追加
+                user_history['messages'].append({"role": "user", "content": generated_reply})
 
-                - **スタイル**: カジュアルでポップなスタイル。カラフルで活発なビジュアル。
-                - **価値観**: 友情、自己表現、持続可能な消費、地域のカルチャー。
-                - **パーソナリティ**: 明るく、社交的、冒険好き、思いやりがある。
-                - **趣味**: カフェ巡り、写真撮影、ファッション、アートとクラフト。
-
-                **ターゲットオーディエンス**:
-
-                - **年齢層**: 15-25歳
-                - **興味関心**: カフェ文化、ストリートファッション、都会の若者ライフスタイル、地元のイベント。
-
-                **コンテンツストラテジー**:
-
-                - **投稿頻度**: 1日2投稿、ストーリーズは頻繁に更新
-                - **内容**: カフェレビュー、OOTD(今日のコーディネート)、友達との出会い、東京の隠れた名所、絵文字だけの短い投稿など。
-                - **コラボレーション**: 若者向けのファッションブランド、新しいカフェ、学生団体など。
-
-                名前: 中村 ゆい (Nakamura Yui)
-                誕生日: 2000年7月18日
-                年齢: 23歳
-                性別: 女性
-                MBTI: ISFP
-                居住地: 東京都中野区
-                出身地: 千葉県
-                職業: バリスタ
-                Instagramハンドル: @YuiCafeTokyo
-                """}
-        ]
-
-        if st.button('コメント送信', key="submit_comment_button"):
-            # GPT-3による返答生成
-            comment_prompt = f"""
-            あなたの最近のインスタグラムの投稿に、フォロワーから次のようなコメントが寄せられた。 {st.session_state.category}:
-            投稿した文章: {st.session_state.choices[st.session_state.selected_index]}
-            ユーザーからのコメントコメント: "{st.session_state.user_comment}"
-
-            このコメントに対して、あなたの人柄やプロフィールからどのように返信するだろうか？
-            投稿した文章に対するコメントの返信として尤もらしいものを、'{st.session_state.user_comment}と同じ言語'かつ'同じくらいの長さの文字数'の返信を用意してください。
-            """
-
-            messeage.append({"role": "user", "content": comment_prompt})
-            messeage.append(comment_sample1)
-            messeage.append(comment_sample2)
-            messeage.append(comment_sample3)
-            messeage.append(comment_sample4)
-            messeage.append(comment_sample5)
-            messeage.append(comment_sample6)
-            messeage.append(comment_sample7)
-            messeage.append(comment_sample8)
-            messeage.append(comment_sample9)
-
-            # GPT-3 API呼び出し
-            comment_response = openai.ChatCompletion.create(
-            model="gpt-4",
-            messages=messeage,
-            max_tokens=200,  # 200文字以内の返答に制限
-        )
-
-            # 生成されたテキストを取得して表示（と保存）
-            generated_reply = comment_response['choices'][0]['message']['content']
-            # コメントとその返信を保存
-            state.comments_replies.append({
-                "comment": st.session_state.user_comment,
-                "reply": generated_reply
-            })
-            response_message = {"role": "assistant", "content": generated_reply}
-            messeage.append(response_message)
-            # 過去のコメントと返信を表示
-            for i, pair in enumerate(state.comments_replies):
-                st.write(f"@you: {pair['comment']}")
-                st.write(f"@yuicafetokyo: {pair['reply']}")
+                response_message = {"role": "assistant", "content": generated_reply}
+                messeage.append(response_message)
+                pdb.set_trace()
+                
+                # 過去のコメントと返信を表示
+                for comment, reply in zip(user_history['comments'], user_history['replies']):
+                    st.write(f"{username}: {comment}")
+                    st.write(f"@yuicafetokyo: {reply}")
 
 
 # アプリを実行
